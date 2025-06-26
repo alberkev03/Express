9bdd2d334dd31cf5dcd21cfc37cbf29efad3e368
alberkev03 <kevinalberto.62@protonmail.com>
2025-06-26 15:50:04 -0300

    updated front

--- frontend/main.js
+++ frontend/main.js
@@ -1,8 +1,14 @@
 const backData = document.querySelector('#backend-data')
-fetch ('http://localhost:3001/api/books')
-    .then(response => response.json())
-    .then(data => {
-        const list = document.createElement('ul')
+const backFilter = document.querySelector('.backend-filter')
+const filterForm = document.querySelector('#filter-by-id')
+const filterName = document.querySelector('#name')
+
+const list = document.createElement('ul')
+
+function showAllData () {
+    fetch ('http://localhost:3001/api/books')
+        .then(response => response.json())
+        .then(data => {
         backData.append(list)
         data.payload.forEach(b => {
             console.log(b);
@@ -12,5 +18,24 @@ fetch ('http://localhost:3001/api/books')
                                 <li>Páginas: ${b.paginas}</li>
                                 <br>`
             list.append(listUnit)
-        })
+    })})
+}
+
+filterForm.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    backFilter.innerHTML = showDataById(Number(filterName.value))
+})
+
+function showDataById (id) {
+    fetch (`http://localhost:3001/api/books/find/${id}`)
+    .then (response => response.json())
+    .then (data => {
+            data.payload.forEach(b => {
+            backFilter.innerHTML = `<div>
+                                        <li>Libro: ${b.nombre}</li>
+                                        <li>Autor: ${b.autor}</li>
+                                        <li>Páginas: ${b.paginas}</li>
+                                    </div>`
+            })
     })
+}
